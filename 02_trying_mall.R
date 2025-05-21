1240ccf2a85b7aa8da797d9158761b9db23e0fb0
MWypych <wypych.mic@gmail.com>
2025-05-21 12:51:40 +0200

    try to work with optimizing mall and craping

--- 02_trying_mall.R
+++ 02_trying_mall.R
@@ -1,4 +1,4 @@
-ollamar::pull("gemma3:4b")
+ollamar::pull("llama3.2")
 
 ollamar::test_connection()
 ollamar::list_models()
@@ -13,30 +13,49 @@ comments <-
 
 library(mall)
 
-llm_use("ollama", "gemma3:4b")
+llm_use("ollama", "llama3.2:latest", seed = 100, temperature = 0)
 
 #some basic sentiment
 
-sentiment <- threads |>
-  llm_sentiment(text)
+sentiment <- head_5 |>
+  llm_sentiment(text) |>
+  select(.sentiment)
 
 #summaries?
 
 
 #classify to topics?
 
-threads |>
-  llm_classify(text, c("work", "relationships"))
+head_5 |>
+  llm_classify(text, c("work", "relationships")) |>
+  select(.classify)
 
 
 #some custom topic thingy?
 
 my_prompt <- paste(
-  "Answer a question.",
-  "Return only the answer, no explanation",
-  "Acceptable answers are 'yes', 'no'",
-  "Answer this about the following text, is this a text funny?:"
+  "Below is a representative set of stories about revenge.",
+  "Please identify the single main topic mentioned in these stories Return a topic name",
+  "The topic name should be short, but descriptive e.g. `work` or `family`. They are all about revenge so do not assign topic revenge",
+  "If you cannot find a good topic label, just say, return `none`",
+  "Return only the answer, no explanation"
 )
 
-threads |>
-  llm_custom(text, my_prompt)
+
+
+head_5 <- threads %>%
+  slice_head(n = 5)
+
+#get number of words
+sapply(gregexpr("[[:alpha:]]+", head_5$text), function(x) sum(x > 0)) |> sum()
+
+head_5 |>
+  llm_custom(text, my_prompt, pred_name = "topic") |>
+  select(text, topic)
+
+
+llm_vec_custom("I got revenge on my boss because he stole my car. I spilled cocoa on his head and sprinkled it with confetti right before his important speech", my_prompt)
+
+
+#ok for now this is going ridiculously slow
+#is there any way to actually speed this up? evaluating 5 texts takes looong. Nowhere near the 1.5 minutes per 20k words in the mall article...
--- source/scrape.R
+++ source/scrape.R
@@ -4,10 +4,25 @@ library(readr)
 #get thread ursl
 revenge_urls <- find_thread_urls(subreddit="ProRevenge", period='all')
 
-#get all the comments
-revenge_comments <- RedditExtractoR::get_thread_content(revenge_urls$url)
 
-#save all threads, recovered threads and comments to csvs
+
+#get all the comments. The API seems to be fairly limiting now so lets try to parse it in chunks
+results <- list()
+i_1 <- 1
+i_2 <- 50
+for(i in 1:18) {
+  revenge_comments <- RedditExtractoR::get_thread_content(revenge_urls[i:i_2,]$url)
+  results <- append(results, list(revenge_comments))
+  i_1 <- i_1 + 50
+  i_2 <- i_2 + 50
+  Sys.sleep(300)
+}
+
+
+#wrangling the list
+
+
+#### saving files
 write_csv(file = "data/threads_all.csv", revenge_urls)
 
 write_csv(file = "data/comments.csv", x = revenge_comments$comments)
